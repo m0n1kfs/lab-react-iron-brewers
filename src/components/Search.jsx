06bdc791a5bb5aa47dfb70f2a08f15a55a92e36b
Monica Fernandez Salinas <monica.fernandez.salinas@gmail.com>
2025-03-26 13:48:56 +0100

    done

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,8 @@
-function Search() {
+function Search({ onSearchTerm }) {
+  const handleChange = (event) => {
+    onSearchTerm(event.target.value);
+  };
+
   return (
     <div className="d-inline-flex justify-content-center align-items-center w-100 p-4">
       <div className="input-group mb-2 w-50">
@@ -10,6 +14,8 @@ function Search() {
         <input
           type="text"
           className="form-control search-bar"
+          onChange={handleChange}
+          placeholder="Search for a beer..."
         />
       </div>
     </div>
